923f29bc1daec872cd7a565f26803466ac4ee39e
nhattybirhanu <nhattybirhanu@gmail.com>
2021-10-05 10:53:07 -0500

    login

---
+++ TableLayoutTest/app/src/main/java/com/inifiny/tablelayouttest/WalMartLogin.kt
@@ -0,0 +1,11 @@
+package com.inifiny.tablelayouttest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WalMartLogin: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.walmart_login);
+    }
+}
